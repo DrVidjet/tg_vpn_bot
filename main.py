7f654222264ffee6e50f295868761ce283429436
awakehns <awake1394hns@gmail.com>
2026-05-17 19:14:15 +0400

    stable version without payment and edit configs

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import configparser
 import sys
 import time
 import fcntl
+import json
 
 ADMIN_ID = 1336949347  # Telegram ID администратора
 SUPPORT = "https://t.me/VidjetVPN"
@@ -17,124 +18,80 @@ user_ids = {}          # telegram_id -> internal_id
 blocked_users = set()
 approved_users = set()
 
-lock_file = open("/tmp/tg_bot.lock", "w")
-
-# Получаем путь к конфигу
-def get_config_path():
-    if getattr(sys, 'frozen', False):
-        base_path = sys._MEIPASS  # Папка с бинарником
-    else:
-        base_path = os.path.dirname(os.path.abspath(__file__))  # Папка с main.py
-    return os.path.join(base_path, 'API.conf')
-
-# Загружаем API ключи
-def load_api_keys():
-    config_path = get_config_path()
-    if not os.path.exists(config_path):
-        raise FileNotFoundError(f"Config file {config_path} not found.")
-    
-    config = configparser.ConfigParser()
-    config.read(config_path)
-    
-    try:
-        api_token = config.get('DEFAULT', 'API').strip('"')
-        return api_token
-    except configparser.NoOptionError:
-        raise ValueError("one of the 'API' keys is missing in config file.")
-
-# # Обработка исключений и перезапуск бота
-# def run_bot():
-#     while True:
-#         try:
-#             print("Bot is successfully started")
-#             bot.polling(none_stop=True, interval=1, timeout=20)
-#         except telebot.apihelper.ApiTelegramException as e:
-#             print(f"Telegram API error: {e}")
-#             time.sleep(5)
-#         except Exception as e:
-#             print(f"An error occurred: {e}")
-#             time.sleep(2)
+LOCK_FILE = "/tmp/tg_vpn_bot.lock"
 
 # Получаем API с конфига
-API_TOKEN = load_api_keys()
+CONFIG_PATH = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    'API.conf'
+)
+
+config = configparser.ConfigParser()
+config.read(CONFIG_PATH)
+
+API_TOKEN = config.get('DEFAULT', 'API').strip('"')
+
 # Инициализируем бота
 bot = telebot.TeleBot(API_TOKEN)
 
-# Глобальное состояние
-user_data = {}
 
-asked_users = set()
+
+#------------------------------------------------------------------------------------------------------------------------------------------------#
+                                                                            ## ФУНКЦИИ  ##
+#------------------------------------------------------------------------------------------------------------------------------------------------#
+
+# Обработка повторного запуска
+def acquire_lock():
+    lock_file = open(LOCK_FILE, "w")
+
+    try:
+        fcntl.flock(lock_file, fcntl.LOCK_EX | fcntl.LOCK_NB)
+    except IOError:
+        print("Bot already running!")
+        sys.exit(1)
+
+    return lock_file
 
 # Обработка пользователей
 def load_users():
-    if not os.path.exists("users.txt"):
+    global user_ids, blocked_users, approved_users
+
+    if not os.path.exists("users.json"):
         return
 
-    with open("users.txt", "r") as f:
-        for line in f:
-            parts = line.strip().split(":")
-            if len(parts) != 3:
-                continue
+    with open("users.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
 
-            uid, tg_id, status = parts
-            tg_id = int(tg_id)
+    for tg_id_str, info in data.items():
+        tg_id = int(tg_id_str)
 
-            user_ids[tg_id] = int(uid)
+        uid = info.get("uid")
+        username = info.get("username")
+        status = info.get("status")
 
-            if status == "block":
-                blocked_users.add(tg_id)
-            elif status == "approved":
-                approved_users.add(tg_id)
+        if uid is not None:
+            user_ids[tg_id] = uid
 
-def save_user(uid, tg_id, status):
-    with open("users.txt", "a") as f:
-        f.write(f"{uid}:{tg_id}:{status}\n")
+        if status == "block":
+            blocked_users.add(tg_id)
+        elif status == "approved":
+            approved_users.add(tg_id)
 
-@bot.message_handler(commands=['start'])
-def start_handler(message):
-    tg_id = message.from_user.id
+def save_user(tg_id, uid, username, status):
+    if os.path.exists("users.json"):
+        with open("users.json", "r", encoding="utf-8") as f:
+            data = json.load(f)
+    else:
+        data = {}
 
-    markup = types.ReplyKeyboardRemove()
-    bot.send_message(chat_id, reply_markup=markup)
+    data[str(tg_id)] = {
+        "uid": uid,
+        "username": username,
+        "status": status
+    }
 
-    # если заблокирован — игнор
-    if tg_id in blocked_users:
-        return
-
-    # если уже approved → сразу меню
-    if tg_id in approved_users:
-        bot.send_message(
-            message.chat.id,
-            "Добро пожаловать 👇",
-            reply_markup=main_menu()
-        )
-        return
-
-    # если уже pending → ждём
-    if tg_id in pending_requests:
-        bot.send_message(message.chat.id, "Жду подтверждения")
-        return
-
-    # иначе → оффер
-    ask_vpn_offer(message)
-
-# Обработчик приглашения в группу телеграм
-@bot.message_handler(content_types=['new_chat_members'])
-def handle_new_chat_member(message):
-    # Проверяем, добавили ли в группу именно бота
-    for new_member in message.new_chat_members:
-        if new_member.id == bot.get_me().id:
-            # Создаем кнопку "Помочь"
-            markup = types.InlineKeyboardMarkup(row_width=1)
-            help_button = InlineKeyboardButton("Получить подписку", url=f"https://t.me/{bot.get_me().username}?start=help")
-            markup.add(help_button)
-
-            # Отправляем сообщение с кнопкой
-            bot.send_message(message.chat.id, 
-                "Спасибо, что добавили меня в группу!\n"
-                "Нажмите кнопку ниже, чтобы перейти в личный чат и начать взаимодействие.",
-                reply_markup=markup
-            )
+    with open("users.json", "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
 
 def support_contact():
     return (
@@ -168,10 +125,10 @@ def ask_vpn_offer(message):
         "🌍 <b>Доступные серверы:</b>\n"
         "• 🇸🇪 Стокгольм ×2\n"
         "• 🇫🇮 Хельсинки ×1\n\n"
-        "Интернет в этих регионах свободный 🏴‍☠️, а распределение автоматическое, выбирать сервер не нужно!\n\n"
+        "🏴‍☠ Интернет в этих регионах свободный, а распределение автоматическое, выбирать сервер не нужно!\n\n"
         "⚡ <b>Преимущества:</b>\n"
         "• Без ограничений по трафику\n"
-        "• Высокая скорость соединения\n"
+        "• Высокая скорость соединения\n\n"
         "📦 <b>После оплаты вы получите:</b>\n"
         "• Конфиг для подключения\n"
         "• Пошаговую инструкцию\n\n"
@@ -180,6 +137,74 @@ def ask_vpn_offer(message):
         parse_mode="HTML",
         reply_markup=markup
     )
+
+# Уведомление администратору
+def send_admin_request(message, internal_id):
+    username = call.from_user.username or "no_username"
+    user_link = f"https://t.me/{username}" if username else "no_username"
+
+    markup = types.InlineKeyboardMarkup()
+    markup.add(
+        types.InlineKeyboardButton("Подтвердить", callback_data=f"approve:{message.from_user.id}"),
+        types.InlineKeyboardButton("Отказать", callback_data=f"deny:{message.from_user.id}")
+    )
+
+    bot.send_message(
+        ADMIN_ID,
+        f"Пользователь {user_link} с id {internal_id} запросил конфиг, жду подтверждения",
+        reply_markup=markup
+    )
+
+def next_uid():
+    return max([int(x) for x in user_ids.values()], default=0) + 1
+
+#------------------------------------------------------------------------------------------------------------------------------------------------#
+                                                                            ## ХЭНДЛЕРЫ  ##
+#------------------------------------------------------------------------------------------------------------------------------------------------#
+@bot.message_handler(commands=['start'])
+def start_handler(message):
+    tg_id = message.from_user.id
+
+    bot.send_message(message.chat.id, "⌛", reply_markup=types.ReplyKeyboardRemove())
+
+    # если заблокирован — игнор
+    if tg_id in blocked_users:
+        return
+
+    # если уже approved → сразу меню
+    if tg_id in approved_users:
+        bot.send_message(
+            message.chat.id,
+            "Добро пожаловать 👇",
+            reply_markup=main_menu()
+        )
+        return
+
+    # если уже pending → ждём
+    if tg_id in pending_requests:
+        bot.send_message(message.chat.id, "Жду подтверждения")
+        return
+
+    # иначе → оффер
+    ask_vpn_offer(message)
+
+# Обработчик приглашения в группу телеграм
+@bot.message_handler(content_types=['new_chat_members'])
+def handle_new_chat_member(message):
+    # Проверяем, добавили ли в группу именно бота
+    for new_member in message.new_chat_members:
+        if new_member.id == bot.get_me().id:
+            # Создаем кнопку "Помочь"
+            markup = types.InlineKeyboardMarkup(row_width=1)
+            help_button = InlineKeyboardButton("Получить подписку", url=f"https://t.me/{bot.get_me().username}?start=start")
+            markup.add(help_button)
+
+            # Отправляем сообщение с кнопкой
+            bot.send_message(message.chat.id, 
+                "Спасибо, что добавили меня в группу!\n"
+                "Нажмите кнопку ниже, чтобы перейти в личный чат и получить конфиг VPN!",
+                reply_markup=markup
+            )
 
 # Обработка ответа
 @bot.message_handler(func=lambda m: m.text in ["Да", "Нет"])
@@ -194,10 +219,10 @@ def handle_offer_response(message):
         return
 
     if tg_id in pending_requests:
-        bot.send_message(message.chat.id, "Жду подтверждения")
+        bot.send_message(message.chat.id, "🕚 Жду подтверждения")
         return
 
-    internal_id = max(user_ids.values(), default=0) + 1
+    internal_id = next_uid()
     user_ids[tg_id] = internal_id
 
     pending_requests[tg_id] = {
@@ -207,7 +232,7 @@ def handle_offer_response(message):
 
     send_admin_request(message, internal_id)
 
-    bot.send_message(message.chat.id, "📨 Заявка отправлена. Ожидайте подтверждения...")
+    bot.send_message(message.chat.id, "🕚 Заявка отправлена. Ожидайте подтверждения...")
 
 # Обработка меню
 @bot.message_handler(func=lambda m: m.text and m.text.strip() in [
@@ -224,7 +249,7 @@ def menu_handler(message):
 
     # 2. pending → ждем
     if tg_id in pending_requests:
-        bot.send_message(message.chat.id, "Жду подтверждения")
+        bot.send_message(message.chat.id, "🕚 Жду подтверждения")
         return
 
     text = message.text.strip()
@@ -233,7 +258,7 @@ def menu_handler(message):
         bot.send_message(message.chat.id, "📦 test")
 
     elif text == "🔄 Продлить подписку":
-        internal_id = user_ids.get(message.from_user.id, max(user_ids.values(), default=0) + 1)
+        internal_id = next_uid()
 
         pending_requests[message.from_user.id] = {
             "id": internal_id,
@@ -246,23 +271,6 @@ def menu_handler(message):
     elif text == "📩 Поддержка":
         bot.send_message(message.chat.id, support_contact())
 
-# Уведомление администратору
-def send_admin_request(message, internal_id):
-    username = message.from_user.username
-    user_link = f"https://t.me/{username}" if username else "no_username"
-
-    markup = types.InlineKeyboardMarkup()
-    markup.add(
-        types.InlineKeyboardButton("Подтвердить", callback_data=f"approve:{message.from_user.id}"),
-        types.InlineKeyboardButton("Отказать", callback_data=f"deny:{message.from_user.id}")
-    )
-
-    bot.send_message(
-        ADMIN_ID,
-        f"Пользователь {user_link} с id {internal_id} запросил конфиг, жду подтверждения",
-        reply_markup=markup
-    )
-
 # Кнопки админа
 @bot.callback_query_handler(func=lambda call: True)
 def admin_actions(call):
@@ -271,23 +279,25 @@ def admin_actions(call):
     tg_id = int(tg_id)
 
     if tg_id not in pending_requests:
-        bot.answer_callback_query(call.id, "Уже обработано")
+        bot.answer_callback_query(call.id, "✅ Уже обработано")
         return
 
     if action == "approve":
         pending_requests.pop(tg_id, None)
         approved_users.add(tg_id)
 
-        uid = user_ids.get(tg_id, max(user_ids.values(), default=0) + 1)
+        uid = user_ids.get(tg_id, next_uid())
         user_ids[tg_id] = uid
 
-        save_user(uid, tg_id, "approved")
+        username = call.from_user.username or "no_username"
+        save_user(tg_id, uid, username, "approved")
 
         bot.send_message(tg_id, "✅ Доступ подтверждён")
 
         bot.send_message(
             tg_id,
-            "Платёж подтверждён! 🎉 Добро пожаловать!",
+            "✅ Платёж подтверждён! 🎉 Добро пожаловать!\n\n"
+            f"Задать вопросы можно в группе, там же публикуются все новости сервиса: {SUPPORT}",
             reply_markup=main_menu()
         )
 
@@ -296,26 +306,31 @@ def admin_actions(call):
 
         blocked_users.add(tg_id)
 
-        uid = user_ids.get(tg_id, max(user_ids.values(), default=0) + 1)
+        uid = user_ids.get(tg_id, next_uid())
         user_ids[tg_id] = uid
 
-        save_user(uid, tg_id, "block")
+        username = call.from_user.username
+        save_user(tg_id, uid, username, "block")
 
         bot.send_message(tg_id, "❌ Вам отказано в предоставлении доступа")
 
-        bot.send_message(call.message.chat.id, "Пользователь заблокирован")
+        bot.send_message(call.message.chat.id, "❌ Пользователь заблокирован")
 
-# Запуск бота
+
+
+#------------------------------------------------------------------------------------------------------------------------------------------------#
+                                                                            ## ЗАПУСК БОТА ##
+#------------------------------------------------------------------------------------------------------------------------------------------------#
+
 if __name__ == '__main__':
-    try:
-        fcntl.flock(lock_file, fcntl.LOCK_EX | fcntl.LOCK_NB)
-    except IOError:
-        print("Bot already running!")
-        sys.exit(1)
+    lock_file = acquire_lock()
+
     try:
         load_users()
-        #run_bot()  # Вызываем функцию для запуска с обработкой ошибок
+
         print("Bot is successfully started")
-        bot.polling(none_stop=True, interval=1, timeout=10)
+
+        bot.infinity_polling(skip_pending=True)
+
     except Exception as e:
         print(f"Fatal error occurred: {e}")
