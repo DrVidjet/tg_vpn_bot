a76ceca35604ba987198b52dafba0ee288a7d0d8
awakehns <awake1394hns@gmail.com>
2026-05-20 17:34:49 +0400

    .

--- main.py
+++ main.py
@@ -767,8 +767,8 @@ def get_monthly_report():
 
     for p in sorted(month_payments, key=lambda x: x["date"], reverse=True)[:15]:
         text += (
-            f"{'─' * 15}\n"
-            f"• {p.get('email', '—')} "
+            f"{'─' * 18}\n"
+            f"• {p.get('email', '—')}\n"
             f"  {p['date'][:16]} | "
             f"{p['amount']}₽ | "
             f"({p['type']})\n"
